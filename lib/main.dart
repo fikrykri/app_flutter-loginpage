21eb2be375a86dfac17b3cf0b330c3f78ee9c014
FikryAS <azizfikry15@gmail.com>
2020-11-06 20:10:12 +0700

    add remove debug label and potraitIUp only

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:aplikasi_loginpage_inisiated/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // mencegah error
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
+}
 
+/**
+ * pada line ke 7 berfungsi agar tampilan pada app pada saat di leadscape tidak akan berubah, namun
+ * akan tetap potrait/ tegak
+ */
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // untuk menghilangkan label debug
       home: MainPage(),
     );
   }
