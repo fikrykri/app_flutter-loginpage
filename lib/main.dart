2f41a1b5b27e170510602fe7d96160aaf1f5e714
FikryAS <azizfikry15@gmail.com>
2020-11-06 17:12:01 +0700

    add main project

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:aplikasi_loginpage_inisiated/main_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
